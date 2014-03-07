d5793e63be9830182148087a7a5662e656b80d4b
kandaj <kandaj@ebi.ac.uk>
2014-03-07 15:18:24 +0000

    protein controller added

--- cellbase-web/web/app/js/controllers/proteinController.js
+++ cellbase-web/web/app/js/controllers/proteinController.js
@@ -1,9 +1,92 @@
 /**
  * Created by jag on 06/03/2014.
  */
-var proteinCtrl = myApp.controller('proteinCtrl', ['$scope', '$rootScope', 'mySharedService', 'CellbaseService', function ($scope, $rootScope, mySharedService, CellbaseService) {
-    $scope.specie = mySharedService.genesSpecie;
+
+myApp2.factory('proteinService', function($rootScope) {
+    var proteinService = {};
+
+    proteinService.message = '';
+
+    proteinService.prepForBroadcast = function(msg) {
+        this.message = msg;
+        this.broadcastItem();
+    };
+
+    proteinService.proteinChange = function(msg) {
+        this.message = msg;
+        $rootScope.$broadcast('broadcastProteinChange');
+    };
+
+    proteinService.broadcastItem = function() {
+        $rootScope.$broadcast('handleBroadcast');
+    };
+
+    return proteinService;
+});
+
+var proteinCtrl = myApp.controller('proteinCtrl', ['$scope', '$rootScope', 'mySharedService', 'CellbaseService','proteinService', function ($scope, $rootScope, mySharedService, CellbaseService, proteinService) {
+
+ $scope.proteinChange = function(msg){
+    proteinService.proteinChange(msg);
+ }
 
 }]);
 
+function proteinSelect($scope, proteinService) {
+    $scope.handleClick = function(msg) {
+        proteinService.prepForBroadcast(msg);
+    };
+
+    $scope.$on('handleBroadcast', function() {
+        $scope.message = proteinService.message;
+    });
+
+    $scope.colors = [
+        {name:'black', shade:'dark'},
+        {name:'white', shade:'light'},
+        {name:'red', shade:'dark'},
+        {name:'blue', shade:'dark'},
+        {name:'yellow', shade:'light'}
+    ];
+    $scope.color = $scope.colors[2];
+
+    $scope.selectedOption  = function(){
+        console.log($scope.color.name)
+        proteinService.prepForBroadcast($scope.color.name);
+    }
+}
+
+function proteinResult($scope, proteinService) {
+    $scope.change = function(){
+        $scope.proteinChange($scope.message);
+    }
+
+    $scope.selectedOption  = function(){
+        proteinService.prepForBroadcast($scope.color.name);
+    }
+    $scope.$on('handleBroadcast', function() {
+        $scope.message =  proteinService.message;
+    });
+    $scope.$on('broadcastProteinChange', function() {
+        $scope.message =  proteinService.message;
+    });
+}
+
+function proteinView($scope, proteinService) {
+
+    $scope.change = function(){
+        $scope.proteinChange($scope.message);
+    }
+
+    $scope.$on('handleBroadcast', function() {
+        $scope.message =  proteinService.message;
+    });
+
+    $scope.$on('broadcastProteinChange', function() {
+        $scope.message =  proteinService.message;
+    });
+}
+
 proteinCtrl.$inject = ['$scope', 'mySharedService'];
+proteinCtrl.$inject = ['$scope', 'proteinService'];
+
